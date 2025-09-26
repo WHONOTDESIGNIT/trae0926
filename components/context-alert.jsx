c5d5cc009ac4b0c1c8edd0c64f177a8b5f20786a
WHONOTDESIGNIT <titoupumeu8277@sakana.ds.lib.uw.edu>
2025-09-26 14:52:39 +0800

    Add files via upload

---
+++ components/context-alert.jsx
@@ -0,0 +1,31 @@
+import { getNetlifyContext } from 'utils';
+import { Alert } from './alert';
+import { Markdown } from './markdown';
+
+const noNetlifyContextAlert = `
+For full functionality, either run this site locally via \`netlify dev\`
+([see docs](https://docs.netlify.com/cli/local-development/")) or deploy it to Netlify.
+`;
+
+export function ContextAlert(props) {
+    const { addedChecksFunction, className } = props;
+    // 将 getNetlifyContext 调用移到组件内部以避免 workUnitAsyncStorage 错误
+    const ctx = getNetlifyContext();
+
+    let markdownText = null;
+    if (!ctx) {
+        markdownText = noNetlifyContextAlert;
+    } else if (addedChecksFunction) {
+        markdownText = addedChecksFunction(ctx);
+    }
+
+    if (markdownText) {
+        return (
+            <Alert className={className}>
+                <Markdown content={markdownText} />
+            </Alert>
+        );
+    } else {
+        return <></>;
+    }
+}
